f864e2192af4415bf39021987d644994329789e1
Steve Renaker <renaker@yahoo.com>
2013-04-01 02:59:28 -0700

    trying mongolab uri again

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,15 @@
 require 'rubygems'
+require "mongo_mapper"
+ 
+regex_match = /.*:\/\/(.*):(.*)@(.*):(.*)\//.match(ENV['MONGOLAB_URI'])
+host = regex_match[3]
+port = regex_match[4]
+db_name = regex_match[1]
+pw = regex_match[2]
+ 
+MongoMapper.connection = Mongo::Connection.new(host, port)
+MongoMapper.database = db_name
+MongoMapper.database.authenticate(db_name, pw)
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
