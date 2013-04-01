0b9e5f56c7e67ed8018b02866ef96df6f98287ce
Steve Renaker <renaker@yahoo.com>
2013-04-01 14:19:08 -0700

    fiddling with boot.rb so that it will work for both development and production

--- config/boot.rb
+++ config/boot.rb
@@ -1,15 +1,18 @@
 require 'rubygems'
-require "mongo_mapper"
+
  
-regex_match = /.*:\/\/(.*):(.*)@(.*):(.*)\//.match(ENV['MONGOLAB_URI'])
-host = regex_match[3]
-port = regex_match[4]
-db_name = regex_match[1]
-pw = regex_match[2]
- 
-MongoMapper.connection = Mongo::Connection.new(host, port)
-MongoMapper.database = db_name
-MongoMapper.database.authenticate(db_name, pw)
+if ENV["RAILS_ENV"] == 'production'  
+  regex_match = /.*:\/\/(.*):(.*)@(.*):(.*)\//.match(ENV['MONGOLAB_URI'])
+  host = regex_match[3]
+  port = regex_match[4]
+  db_name = regex_match[1]
+  pw = regex_match[2]
+  
+
+  MongoMapper.connection = Mongo::Connection.new(host, port)
+  MongoMapper.database = db_name
+  MongoMapper.database.authenticate(db_name, pw)
+end  
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
